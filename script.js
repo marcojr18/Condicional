98861698201ee6a3407f06c537e1f49a139b0b17
marcojunior18 <marcoantoniodeoliveirajunior@gmail.com>
2024-10-16 15:44:11 -0300

    condicional

---
+++ script.js
@@ -0,0 +1,29 @@
+//Condicionais são uma forma de fazer o computador executar decisões!
+
+//if significa "se"
+//else significa "senão"
+
+// if (condição) {codigo que será executado}
+
+//promt pergunta algo ao usuario
+
+//let nome = prompt("Qual é o seu nome ?");
+// alert é usado para exibir uma mensagem 
+//alert("seja bem vindo(a)" + nome);
+
+//let idade = prompt("Quantos anos você tem?")
+//if(idade >= 18) {
+   // alert("você já pode tirtar sua habilitação") 
+   // {else{
+    //alert("você ainda não tem idade para ser habilitado")
+    
+
+    let gostarDepizza = prompt("você gosta de pizza? responda (sim ou não)");
+    if(gostarDepizza=== "sim"){
+        alert("que ótimo Vamos comer juntos")
+    }else{
+            alert("tudo bem, talvez você goste de outra comida")
+
+        
+    }
+
